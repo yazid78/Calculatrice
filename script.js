159f4ca4f0d9dac8d24659dd3eeccc364e45aa52
Yazid <yazid780@outlook.fr>
2024-04-19 12:54:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculateResult() {
+  display.value = eval(display.value);
+}
